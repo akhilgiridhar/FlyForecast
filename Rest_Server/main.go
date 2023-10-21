e1711c47183b41d4a406e34b817c51e607fc209f
Akhil Giridhar <akhilgiridhar007@gmail.com>
2023-10-21 14:08:54 -0500

    Base outline of REST Server

--- Rest_Server/main.go
+++ Rest_Server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getPrediction(data Input) Ouput {
+	// Make HTTP request to  ML model API and pass in the data
+	// Parse the response
+	// Return the response
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := gin.Default()
+
+	r.POST("/predict", func(c *gin.Context) {
+		var input Input
+		if err := c.ShouldBindJSON(&input); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		output := getPrediction(input)
+		c.JSON(http.StatusOK, output)
+	})
+
+	r.Run()
 }
