53f374271511ec9f3cf060099eef6f394ca71fc6
Akhil Giridhar <akhilgiridhar007@gmail.com>
2023-10-21 22:01:50 -0500

    Fully integrated API for real-time inputs

--- rest_server/go_app/main.go
+++ rest_server/go_app/main.go
@@ -8,8 +8,9 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"net/url"
 	"os"
-	"os/exec"
+	"strconv"
 )
 
 type Input struct {
@@ -24,29 +25,49 @@ type Output struct {
 	Delay int `json:"delay"`
 }
 
+const api_key = "f69006857c004d90ad0222208232110"            // Replace with your actual API key
+const api_url = "http://api.weatherapi.com/v1/forecast.json" // Replace with the API's URL
+
 func callParsedata(city, time string) Input {
-	cmd := exec.Command("python", "inputparsing.py", "parsedata", time, city)
-	var out bytes.Buffer
-	cmd.Stdout = &out
+	var result Input
 
-	err := cmd.Run()
+	// Create URL with parameters
+	u, _ := url.Parse(api_url)
+	q := u.Query()
+	q.Set("key", api_key)
+	q.Set("q", city)
+	u.RawQuery = q.Encode()
+
+	resp, err := http.Get(u.String())
 	if err != nil {
-		fmt.Println("Error executing Python script:", err)
-		return Input{}
+		fmt.Println("Failed to make request:", err)
+		return result
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		fmt.Println("Request failed with status code:", resp.StatusCode)
+		return result
 	}
 
-	// The output from the Python script is stored in 'out'
-	// Unmarshal the JSON data into the Go struct
-	pythonOutput := out.String()
+	body, _ := io.ReadAll(resp.Body)
+	var data map[string]interface{}
+	json.Unmarshal(body, &data)
 
-	// Unmarshal the JSON data into the Go struct
-	var input Input
-	err = json.Unmarshal([]byte(pythonOutput), &input)
-	if err != nil {
-		fmt.Println("Error unmarshaling JSON:", err)
-		return Input{}
-	}
-	return input
+	forecast := data["forecast"].(map[string]interface{})
+	day := forecast["forecastday"].([]interface{})[0]
+	hourData := day.(map[string]interface{})["hour"].([]interface{})
+
+	timeInt, _ := strconv.Atoi(time)
+	hour := hourData[timeInt].(map[string]interface{})
+
+	result.Dew = int(hour["dewpoint_f"].(float64))
+	result.Slp = int(hour["pressure_mb"].(float64))
+	result.Tmp = int(hour["temp_c"].(float64))
+	result.Vis = int(hour["vis_km"].(float64) * 1000)
+	result.Wnd_speed = int(hour["wind_mph"].(float64))
+
+	return result
 }
 
 func getPrediction(data Input) Output {
@@ -90,6 +111,7 @@ func main() {
 	fmt.Println("Time:", time)
 
 	input = callParsedata(city, time)
+	fmt.Println(input)
 
 	output := getPrediction(input)
 	if output.Delay == 0 {
